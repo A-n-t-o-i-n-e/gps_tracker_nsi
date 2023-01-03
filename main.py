43a9924d251169ba6153998429fac80635ffd298
A-n-t-o-i-n-e <98612177+A-n-t-o-i-n-e@users.noreply.github.com>
2023-01-03 14:47:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 # Ce code envoie un signal gps lorsque le capteur est dépalcé
 
-pass
+from microbit import uart, button_a, button_b, accelerometer
+
+# init gps
+microbit.uart.init(baudrate=9600, bits=8, parity=None, stop=1, tx=None, rx=None)
+
+while True:
+    
+    if button_b.is_pressed():
+        pass
+        
+    elif button_a.was_pressed():
+        if accelerometer.was_gesture('shake'):
+            uart.read([8])
